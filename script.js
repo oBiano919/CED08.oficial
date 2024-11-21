9f02e11d662f05452f7507b7d69d5423b5228d89
o biano <fabinnho.10sul@gmail.com>
2024-11-21 09:44:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const navmenu = document.querySelector('.nav-menu');
+    navLinks.classList.toggle('active');
+}
+
+ function toggleMenu() {
+    document.querySelector('.nav-menu').classList.toggle('open');
+}
